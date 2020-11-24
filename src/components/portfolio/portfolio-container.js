5e9350089243d0be658ea80354100d6903a7fecc
vazquezJordy <jordyvazgol@gmail.com>
2020-11-24 00:45:39 -0700

    Added a firlter handler button that show you tech companies depending on the category

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,62 @@
+import React, {Component} from "react"
+
+import PortfolioItem from "./portfolio-Item"
+
+
+export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            data: [
+               {title: "DOMO", catagory:"Tech"}, 
+               {title:" Pluralsight", catagory:"Learning"},
+               {title: "Divvy", catagory: "Enterprise" },
+               {title: "MX", catagory: "Enterprise"},
+               {title: "Jobnimbus", catagory: "Enterprise"}
+            ]
+        };
+
+        // this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this)
+        this.handleFilter = this.handleFilter.bind(this);
+    }
+
+    portfolioItems() {
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={"google.com"} />;
+        });
+    }
+
+    // handlePageTitleUpdate()  {
+    //     this.setState({
+    //         pageTitle: "Something else"
+    //     });
+    // }
+
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.catagory === filter;
+            })
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2> {this.state.pageTitle} </h2>
+
+                
+
+                <button onClick={() => this.handleFilter('Enterprise')}>Enterprice</button>
+                <button onClick={() => this.handleFilter('Learning')}>Learning</button>
+                <button onClick={() => this.handleFilter('Tech')}>Tech</button>
+
+                {this.portfolioItems()}
+
+                {/* <button onClick={this.handlePageTitleUpdate}>Change Title</button> */}
+            </div>
+        )
+    }
+}
