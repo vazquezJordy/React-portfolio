5d8bb44b5902113d25ff009c73179142756beeca
vazquezJordy <jordyvazgol@gmail.com>
2020-11-29 19:35:22 -0700

    Added a bunch of data with images to our api, api renders perfecly even the url for the web pull up

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -37,8 +37,7 @@ export default class PortfolioContainer extends Component {
 
     portfolioItems() {
         return this.state.data.map(item => {
-            
-            return <PortfolioItem title={item.name} url={item.url} slug={item.id}/>;
+            return <PortfolioItem key={item.id}title={item.name} url={item.url} slug={item.id}/>;
         });
     }
 
