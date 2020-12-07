6d8b7ce2d7a0b107ef25683ea09946c729d7897e
vazquezJordy <jordyvazgol@gmail.com>
2020-12-06 22:27:59 -0700

    added a handler to update the state of email once and email is being inserted also added 2 h2 tags to see how it is updating its self

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+    constructor(probs){
+        super(probs);
+
+        this.state= {
+            email: "",
+            password: ""
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        console.log("handlesubmint", event)
+    }
     render() {
         return (
             <div>
                 <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
 
-                <form>
-                    <input type="text" />
-                    <input type="password" />
+                <h2>{this.state.email}</h2>
+                <h2>{this.state.password}</h2>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                    type="email"
+                    name="email"
+                    placeholder="Your email goes here"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    />
+                    
+                    <input 
+                    type="password"
+                    name="password"
+                    placeholder="Your password goes here"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    />
+                    
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
                 </form>
 
             </div>
