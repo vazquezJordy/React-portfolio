5f7662788e0efcba3cf24e84a81118832bc2fac3
vazquezJordy <jordyvazgol@gmail.com>
2020-12-09 23:21:12 -0700

    added a link to contact.js file that will take you to the auth.js file and from there you can log in into admin

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 export default function() {
         return (
             <div>
-               Contact
+               <h1>Contact</h1>
+               <div>
+                   <Link to="/auth">Admin Log in</Link>
+               </div>
             </div>
         );
 }
