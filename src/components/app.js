d1af4c2db3cd355867d3b88fc49372faa3b073cd
vazquezJordy <jordyvazgol@gmail.com>
2020-12-14 21:51:08 -0700

    added a route guard to my blog page where only you will be able to access it if user islogged in else it will say page not found

--- src/components/app.js
+++ src/components/app.js
@@ -66,6 +66,10 @@ export default class App extends Component {
     this.checkLoginStatus();
   }
 
+  authorizedPages() {
+    return [<Route exact path="/blog" component ={Blog} />];
+  }
+
   render() {
     
     return (
@@ -93,7 +97,7 @@ export default class App extends Component {
 
               <Route exact path="/about-me" component ={About} />
               <Route exact path="/contact" component ={Contact} />
-              <Route exact path="/blog" component ={Blog} />
+              {this.state.loggedInStatus === "LOGGED_IN" ?  this.authorizedPages() : null}
               <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
               <Route component={NoMatch} />
             </Switch>
