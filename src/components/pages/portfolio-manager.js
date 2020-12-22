d257fe848d0c356ae231a72ddc6923f3466f292d
vazquezJordy <jordyvazgol@gmail.com>
2020-12-21 22:01:29 -0700

    i added a new file to portfolio that will be for the sidebar in the portfolio manager. it takes in probs of data to be able to render in name, image, and id.

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import PortfolioSidebarList from '../portfolio/portfolio-sidebar-list';
+
 
 export default class PortfolioManager extends Component {
   constructor(){
@@ -40,7 +42,7 @@ export default class PortfolioManager extends Component {
         </div>
 
         <div className="right-column">
-          <h1>Portfolio sidebar...</h1>
+          <PortfolioSidebarList data={this.state.portfolioItems}/>
          
         </div>
       </div>
