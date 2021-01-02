e5fb8a5b505dcaeaf4314879e8fe0b98e0c80780
vazquezJordy <jordyvazgol@gmail.com>
2021-01-01 23:45:07 -0700

    added the rest of the function to be able to delete the portfolio uploaded while also having it update on our site at the same time.

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -5,6 +5,7 @@ import PortfolioSidebarList from '../portfolio/portfolio-sidebar-list';
 import PortfolioForm from '../portfolio/portfolio-form';
 
 
+
 export default class PortfolioManager extends Component {
   constructor(){
     super();
@@ -18,7 +19,19 @@ export default class PortfolioManager extends Component {
   }
 
   handleDeleteClick(portfolioItem){
-    console.log("handleDeleteClick", portfolioItem)
+    axios.delete(`https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`, 
+    {withCredentials: true}
+    ).then(response => {
+      this.setState({ 
+        portfolioItems: this.state.portfolioItems.filter(item => {
+          return item.id !== portfolioItem.id;
+        })
+      })
+      return response.data;
+    })
+    .catch(error => {
+      console.log("handleDleteClick", error);
+    });
   }
 
   handleSuccessfulForSubmission(portfolioItem) {
