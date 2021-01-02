039fdffec58eed6a340efa16eccf16aa039a45e4
vazquezJordy <jordyvazgol@gmail.com>
2021-01-02 00:21:30 -0700

    added fontawseome libraries for icons. i was also able to add 2 icons one for trash and sign out. and within the app component iwas able to call from the library those icons.

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from 'axios';
+import { library} from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faTrash, faSignOutAlt } from "@fortawesome/free-solid-svg-icons"
 
 import NavigationContainer from './navigation/navigation-container'
 import Home from './pages/home';
@@ -12,6 +15,7 @@ import PortfolioDetail  from "./portfolio/porfolio-detail";
 import Auth  from "./pages/auth";
 import NoMatch  from "./pages/no-match";
 
+library.add(faTrash, faSignOutAlt);
 
 
 export default class App extends Component {
