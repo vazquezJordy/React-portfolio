d8ddd897de56bd8aaff2d480742c62605eb79ed5
vazquezJordy <jordyvazgol@gmail.com>
2021-01-02 23:59:48 -0700

    added more to setstate and now when you edit and click save you can see that it updates the current file even though it may seem it created a new one. once you refresh you can see the update

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -18,6 +18,9 @@ export default class PortfolioForm extends Component {
       thumb_image: "",
       banner_image: "",
       logo: "",
+      editMode: false,
+      apiUrl: "https://jvazquez.devcamp.space/portfolio/portfolio_items",
+      apiAction: 'post'
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -55,7 +58,10 @@ export default class PortfolioForm extends Component {
       description: description || "",
       category: category || "eCommerce",
       position: position || "",
-      url: url || ""
+      url: url || "",
+      editMode: true,
+      apiUrl: `https://jvazquez.devcamp.space/portfolio/portfolio_items/${id}`,
+      apiAction: 'patch'
       });
     }
   }
@@ -122,12 +128,12 @@ export default class PortfolioForm extends Component {
   }
 
   handleSubmit(event) {
-    axios
-      .post(
-        "https://jvazquez.devcamp.space/portfolio/portfolio_items",
-        this.buildForm(),
-        { withCredentials: true }
-      )
+    axios({
+      method: this.state.apiAction,
+      url: this.state.apiUrl,
+      data: this.buildForm(),
+      withCredentials: true
+    })
       .then((response) => {
         this.props.handleSuccessfulForSubmission(response.data.portfolio_item);
 
