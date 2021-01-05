1de4c189836cb844a8b0eec84af70e7fb7482c0e
vazquezJordy <jordyvazgol@gmail.com>
2021-01-04 19:39:36 -0700

    i had a bug to where if editing the image added in editing mode would not show up and this was fixed by changing the name in didupdate component

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -69,9 +69,9 @@ export default class PortfolioForm extends Component {
         editMode: true,
         apiUrl: `https://jvazquez.devcamp.space/portfolio/portfolio_items/${id}`,
         apiAction: "patch",
-        thumb_image: thumb_image_url || "",
-        banner_image: banner_image_url || "",
-        logo: logo_url || "",
+        thumb_image_url: thumb_image_url || "",
+        banner_image_url: banner_image_url || "",
+        logo_url: logo_url || "",
       });
     }
   }
@@ -227,9 +227,9 @@ export default class PortfolioForm extends Component {
         </div>
 
         <div className="image-uploaders">
-          {this.state.thumb_image && this.state.editMode ? (
+          {this.state.thumb_image_url && this.state.editMode ? (
             <div className="portfolio-manager-image-wrapper">
-              <img src={this.state.thumb_image}></img>
+              <img src={this.state.thumb_image_url}></img>
 
               <div className="image-removal-link">
                 <a onClick={() => this.deleteImage("thumb_image")}>
@@ -249,9 +249,9 @@ export default class PortfolioForm extends Component {
             </DropzoneComponent>
           )}
 
-          {this.state.banner_image && this.state.editMode ? (
+          {this.state.banner_image_url && this.state.editMode ? (
             <div className="portfolio-manager-image-wrapper">
-              <img src={this.state.banner_image}></img>
+              <img src={this.state.banner_image_url}></img>
 
               <div className="image-removal-link">
                 <a onClick={() => this.deleteImage("banner_image")}>
@@ -271,9 +271,9 @@ export default class PortfolioForm extends Component {
             </DropzoneComponent>
           )}
 
-          {this.state.logo && this.state.editMode ? (
+          {this.state.logo_url && this.state.editMode ? (
             <div className="portfolio-manager-image-wrapper">
-              <img src={this.state.logo}></img>
+              <img src={this.state.logo_url}></img>
 
               <div className="image-removal-link">
                 <a onClick={() => this.deleteImage("logo")}>
