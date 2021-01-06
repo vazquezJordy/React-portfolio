85a2bda7f9536556fdd536bf21581641b4f5cacd
vazquezJordy <jordyvazgol@gmail.com>
2021-01-05 22:48:03 -0700

    when you click on a blog title it will take you to the blog item and show you whats in it and so far you can just the title

--- src/components/app.js
+++ src/components/app.js
@@ -10,6 +10,7 @@ import Home from './pages/home';
 import About  from "./pages/about";
 import Contact from "./pages/contact";
 import Blog  from "./pages/blog";
+import BlogDetail from "./pages/blog-detail"
 import PorfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail  from "./portfolio/porfolio-detail";
 import Auth  from "./pages/auth";
@@ -112,6 +113,7 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
+              <Route path="/b/:slug" component={BlogDetail} />
               {this.state.loggedInStatus === "LOGGED_IN" ? (
                 this.authorizedPages()
               ) : null}
