5e9b62bdd822f833ff7b82b55d78512d32194413
vazquezJordy <jordyvazgol@gmail.com>
2021-01-06 23:51:45 -0700

    now when you scroll to the bottom it will call the api and get the items on the second page and load them. if there is no more items on that page it wont make the api call.

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -22,9 +22,12 @@ class Blog extends Component {
 
   activateInfiniteScroll() {
       window.onscroll = () => {
-
+          if (this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
+              return;
+          }
+         
          if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-             console.log("get more posts");
+             this.getBlogItems();
          }
       }
   }
@@ -36,12 +39,13 @@ class Blog extends Component {
 
 
     axios
-      .get("https://jvazquez.devcamp.space/portfolio/portfolio_blogs", {
+      .get(`https://jvazquez.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`, {
         withCredentials: true,
       })
       .then((response) => {
+        console.log("getting", response.data)
         this.setState({
-          blogItems: response.data.portfolio_blogs,
+          blogItems:this.state.blogItems.concat(response.data.portfolio_blogs),
           totalCount: response.data.meta.total_records,
           isLoading: false
         });
