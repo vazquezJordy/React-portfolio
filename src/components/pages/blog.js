3bc3389a994c53d19f8b93c5cd547955cb3944bd
vazquezJordy <jordyvazgol@gmail.com>
2021-01-07 19:29:56 -0700

    we had a bug  that if blog page loaded and you didn't scroll to the bottom and changed into the home page and scrolled to the bottom it would make a nother call to blogitems and we get a error returned. we fixed this by adding a evernt listener that listens for scroll

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -17,18 +17,19 @@ class Blog extends Component {
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
-    this.activateInfiniteScroll();
+    this.onScroll = this.onScroll.bind(this);
+    window.addEventListener("scroll", this.onScroll, false);
   }
 
-  activateInfiniteScroll() {
-      window.onscroll = () => {
+  onScroll() {
+      
           if (this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
               return;
           }
          
          if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
              this.getBlogItems();
-         }
+         
       }
   }
 
@@ -57,6 +58,10 @@ class Blog extends Component {
 
   componentWillMount() {
     this.getBlogItems();
+  }
+
+  componentWillUnmount() {
+      window.removeEventListener("scroll", this.onScroll, false);
   }
 
   render() {
