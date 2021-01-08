350dd59b808bda1bde77a1edbd82d9f8274bfe55
vazquezJordy <jordyvazgol@gmail.com>
2021-01-07 23:00:19 -0700

    passed modal is open as blogmodaldel being updated if that is true the modal will show after open madal has been click on.

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -5,9 +5,10 @@ export default class BlogModal extends Component {
     constructor(props) {
         super(props);
     }
+    
     render() {
         return (
-            <ReactModal isOpen={true}>
+            <ReactModal isOpen={this.props.modalIsOpen}>
                 <h1>I'm a MODAL</h1>
             </ReactModal>
         )
