de7c86aff850925f21920fd47e1ca74c8099eb44
vazquezJordy <jordyvazgol@gmail.com>
2021-01-08 18:37:15 -0700

    now when you submit in modal the status gets updated and appended to a new form data and gets posted using axios post. you can see that it was succesfull because it returns in the console but you can also see in in devspace

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 
 export default class BlogForm extends Component {
   constructor(props) {
@@ -13,7 +15,22 @@ export default class BlogForm extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  buildForm(){
+      let formData = new FormData();
+
+      formData.append("portfolio_blog[title]", this.state.title);
+      formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+
+      return formData;
+  }
+
   handleSubmit() {
+    axios.post("https://jvazquez.devcamp.space/portfolio/portfolio_blogs", this.buildForm(), {withCredentials: true}
+    ).then(response => {
+        this.props.handleSuccessfullFormSubmission(response.data);
+    }).catch(error => {
+        console.log("handle submit for blog error", error)
+    })
     this.props.handleSuccessfullFormSubmission(this.state);
     event.preventDefault();
   }
