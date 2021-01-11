05acdc0084fbb461e0d5bf33b6d248364a415a5c
vazquezJordy <jordyvazgol@gmail.com>
2021-01-10 22:50:54 -0700

    added a handler that will handle the content added from rich text editor who is able to use it to set state

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -10,10 +10,16 @@ export default class BlogForm extends Component {
     this.state = {
       title: "",
       blog_status: "",
+      content: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this);
+  }
+
+  handleRichTextEditorChange(content) {
+    this.setState({ content }); 
   }
 
   buildForm() {
@@ -21,6 +27,7 @@ export default class BlogForm extends Component {
 
     formData.append("portfolio_blog[title]", this.state.title);
     formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+    formData.append("portfolio_blog[content]", this.state.content);
 
     return formData;
   }
@@ -70,8 +77,11 @@ export default class BlogForm extends Component {
         </div>
 
         <div className="one-column">
-          <RichTextEditor />
+          <RichTextEditor  
+          handleRichTextEditorChange={this.state.handleRichTextEditorChange}
+          />
         </div>
+        
 
         <button className="btn">Save</button>
       </form>
