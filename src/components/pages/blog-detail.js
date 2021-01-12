bbd3e31e4c27157d6e9b15ceafdf841cae56466b
vazquezJordy <jordyvazgol@gmail.com>
2021-01-11 22:25:55 -0700

    brought in a new pagackage and it was used to parse html and make it look nice. I also worked on the the image inside of detail to where if there is nothing there it will return nothing instead of having a grey box there

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import ReactHtmlParser from "react-html-parser";
+import BlogFeaturedImage from "../blog/blog-featured-image";
 
 export default class BlogDetail extends Component {
   constructor(props) {
@@ -42,15 +44,11 @@ export default class BlogDetail extends Component {
         <div className="content-container">
           <h1>{title}</h1>
 
-          <div className="featured-image-wrapper">
-            <img src={featured_image_url} />
-          </div>
+          <BlogFeaturedImage img={featured_image_url} />
 
-          <div className="content">{content}</div>
-          
+          <div className="content">{ReactHtmlParser(content)}</div>
         </div>
       </div>
-
     );
   }
 }
