e863d5922506aa86640eedd945fb908797ba2055
vazquezJordy <jordyvazgol@gmail.com>
2021-01-14 21:17:17 -0700

    we passed to element to the rich text called edit mode and it status and content to edit which to took in a ternary OP that passed blog content if its true. in rich text we called component will mount and if edit mode and contenTotEdit were true it render the contont onto the form.

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { EditorState, convertToRaw } from "draft-js";
+import { EditorState, convertToRaw, ContentState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 import htmlToDraft from "html-to-draftjs";
@@ -17,6 +17,16 @@ export default class RichTextEditor extends Component {
     this.uploadFile = this.uploadFile.bind(this);
   }
 
+  componentWillMount() {
+    if (this.props.editMode && this.props.contentToEdit) {
+      const blocksFromHtml = htmlToDraft(this.props.contentToEdit);
+      const {contentBlocks, entityMap } = blocksFromHtml;
+      const contentState = ContentState.createFromBlockArray(contentBlocks, entityMap);
+      const editorState = EditorState.createWithContent(contentState);
+      this.setState({editorState})
+
+    }
+  }
 
   onEditorStateChange(editorState) {
     this.setState(
