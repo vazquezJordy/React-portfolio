f2f23e85f0e0f98e62a1c1c19eff0442c317983a
vazquezJordy <jordyvazgol@gmail.com>
2021-01-17 23:27:59 -0700

    to portfoli detail made it into  class component and made an api call to get the items for that specific item

--- src/components/portfolio/porfolio-detail.js
+++ src/components/portfolio/porfolio-detail.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default function(props) {
+
+export default class PorfolioDetail extends Component {
+    constructor(props){
+        super(props);
+
+    }
+
+    getPortfolioItem() {
+        axios.get(`https://jvazquez.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`, {withCredentials: true})
+        .then(response => {
+            console.log("items", response);
+        }).catch(error => {
+            console.log("getting Item detail", error);
+        });
+    }
+    componentWillMount() {
+        this.getPortfolioItem()
+    }
+
+    render() {
         return (
             <div>
-               <h2>Portfolio Detail for {props.match.params.slug}</h2>
-
+                <h2>portfolio detail for  {this.props.match.params.slug}</h2>
             </div>
         );
+    }
 }
