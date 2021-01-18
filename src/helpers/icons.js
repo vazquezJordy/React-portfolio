ab8eb50c4c99a46a83e25dee592e6ff3185447b3
vazquezJordy <jordyvazgol@gmail.com>
2021-01-17 20:27:24 -0700

    brought in a new image and icons and set them to the contact page.

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -5,11 +5,14 @@ import {
     faTrashAlt,
     faSpinner,
     faPlus,
+    faPhone,
+    faCompass,
+    faEnvelope,
   } from "@fortawesome/free-solid-svg-icons";
   import { library } from "@fortawesome/fontawesome-svg-core";
 
   const Icons = () => {
-    return library.add(faTrash, faSignOutAlt, faEdit, faTrashAlt, faSpinner, faPlus);
+    return library.add(faTrash, faSignOutAlt, faEdit, faTrashAlt, faSpinner, faPlus, faPhone, faCompass, faEnvelope);
   };
 
   export default Icons;
