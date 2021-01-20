c183ff190a70920112d05f352408434255a56e4e
vazquezJordy <jordyvazgol@gmail.com>
2021-01-19 22:10:19 -0700

    worked on when clicking on the top button for filter to only show x y or z or return all items. added styling

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,93 +1,96 @@
-import React, { Component } from "react"
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-import PortfolioItem from "./portfolio-Item"
-
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            pageTitle: "Welcome to my portfolio",
-            isLoading: false,
-            currentTime: String(new Date()),
-            data: []
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: [],
+    };
 
-        };
+    this.handleFilter = this.handleFilter.bind(this);
+  }
 
-        // this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this)
-        this.handleFilter = this.handleFilter.bind(this);
+  handleFilter(filter) {
+    if (filter === "CLEAR_FILTERS") {
+      this.getPortfolioItems();
+    } else {
+        this.getPortfolioItems(filter);
     }
+  }
 
-    getPortfolioItems() {
-        axios.get('https://jvazquez.devcamp.space/portfolio/portfolio_items')
-            .then(response => {
-                // handle success
-
-                this.setState({
-                    data: response.data.portfolio_items
-                })
-            })
-            .catch(error => {
-                // handle error
-                console.log(error);
-            })
-    }
-
-    portfolioItems() {
-
-
-        //
-        return this.state.data.map(item => {
-            return (<PortfolioItem
-                key={item.id}
-                item={item}
-
-            />
-            );
-        });
-    }
-
-    // handlePageTitleUpdate()  {
-    //     this.setState({
-    //         pageTitle: "Something else"
-    //     });
-    // }
-    // componentDidMount() {
-
-    //     this.liveTime = setInterval(() => {
-    //         //  console.log('New chat message')
-    //          this.setState({currentTime: String(new Date())})
-    //      },1000
-    //      )
-    //     }
-
-    handleFilter(filter) {
-        this.setState({
-            data: this.state.data.filter(item => {
-                return item.catagory === filter;
-            })
-        });
-    }
-
-    componentDidMount() {
-        this.getPortfolioItems();
-    }
-  
-    render() {
-
-        if (this.state.isLoading) {
-            return <div>Loading ....</div>;
+  getPortfolioItems(filter = null) {
+    axios
+      .get("https://jvazquez.devcamp.space/portfolio/portfolio_items")
+      .then((response) => {
+        if (filter) {
+          this.setState({
+            data: response.data.portfolio_items.filter((item) => {
+              return item.category === filter;
+            }),
+          });
+        } else {
+          this.setState({
+            data: response.data.portfolio_items,
+          });
         }
-        
-        return (
-            <div className="portfolio-items-wrapper">
-                <button className="btn" onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-                <button className="btn" onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-                <button className="btn" onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-                {this.portfolioItems()}
-            </div>
-        )
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  portfolioItems() {
+    return this.state.data.map((item) => {
+      return <PortfolioItem key={item.id} item={item} />;
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
+
+    return (
+      <div className="homepage-wrapper">
+        <div className="filter-links">
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("eCommerce")}
+          >
+            eCommerce
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Scheduling")}
+          >
+            Scheduling
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Enterprise")}
+          >
+            Enterprise
+          </button>
+
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("CLEAR_FILTERS")}
+          >
+            All
+          </button>
+        </div>
+        <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+      </div>
+    );
+  }
 }
