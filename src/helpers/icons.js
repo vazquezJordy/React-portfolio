71dd80dcc0d31b5875b801baf75905f45a90644e
vazquezJordy <jordyvazgol@gmail.com>
2021-01-20 21:15:34 -0700

    worked on syling out the login page and added some icons

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -8,11 +8,12 @@ import {
     faPhone,
     faCompass,
     faEnvelope,
+    faLock,
   } from "@fortawesome/free-solid-svg-icons";
   import { library } from "@fortawesome/fontawesome-svg-core";
 
   const Icons = () => {
-    return library.add(faTrash, faSignOutAlt, faEdit, faTrashAlt, faSpinner, faPlus, faPhone, faCompass, faEnvelope);
+    return library.add(faTrash, faSignOutAlt, faEdit, faTrashAlt, faSpinner, faPlus, faPhone, faCompass, faEnvelope, faLock);
   };
 
   export default Icons;
